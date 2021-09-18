d3d44a80e90a9425c6127f5fdbff9790e510b08b
Ankit <a.ankit2503@gmail.com>
2021-09-18 11:26:03 +0530

    layout add

--- pages/components/Main.jsx
+++ pages/components/Main.jsx
@@ -5,12 +5,12 @@ import "react-toastify/dist/ReactToastify.css";
 export const Main = () => {
   //hooks
   const inputfile = useRef();
+  const select = useRef();
   const [vtitle, setvtitle] = useState("Select A Video File");
   //functions
-  const notify = () => toast("Wow so easy!");
+
   const getinput = () => {
     inputfile.current.click();
-    notify();
   };
 
   return (
@@ -19,12 +19,15 @@ export const Main = () => {
         <div onClick={getinput} className={styles.filearea}>
           <input
             onChange={() => {
-              if (inputfile.current.files[0].type.includes("video"));
-              {
+              const isVideo = inputfile.current.files[0].type.includes("video");
+
+              if (isVideo) {
+                console.log("ia m herr");
                 return setvtitle(
                   inputfile.current.files[0].name.substring(0, 50)
                 );
               }
+              alert("Select a video File");
             }}
             ref={inputfile}
             className={styles.fileinput}
@@ -40,11 +43,29 @@ export const Main = () => {
           <p className={styles.title}>{vtitle}...</p>
         </div>
       </div>
-      <div className="video">
-        <video src="./myvideo.mp4"></video>
+
+      <div>
+        <video className={styles.video} src="./myvideo.mp4"></video>
       </div>
-      <div className="video">
-        <video src="./myvideo.mp4"></video>
+      <div className={styles.select}>
+        <select
+          ref={select}
+          onChange={() => {
+            console.log(select.current.value);
+          }}
+          name="codec"
+          id={styles.codec}
+        >
+          <option value="mp4">mp4</option>
+          <option value="mp2">mp2</option>
+          <option value="mp3">mp1</option>
+          <option value="mp0">mp0</option>
+        </select>
+
+        <button className={styles.convert}>Convert</button>
+      </div>
+      <div>
+        <video className={styles.video} src="./myvideo.mp4"></video>
       </div>
     </div>
   );
